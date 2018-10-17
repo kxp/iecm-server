983168ed3fa64bf2ade96aeba5c00bd7c8b8eb9a
kxp <andre.cachopas@gmail.com>
2018-10-18 00:32:55 +0100

    Finished Nonblocking tcp server

--- src/com/isel/server/TcpNonBlockServer.java
+++ src/com/isel/server/TcpNonBlockServer.java
@@ -1,10 +1,15 @@
 package com.isel.server;
 
+
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,6 +19,8 @@ public final class TcpNonBlockServer implements IServer {
     private boolean running = true;
     private ServerSocketChannel serverSocketChannel;
     private ServerSocket serverSocket;
+    private Selector selector;
+    private ExecutorService threadPool;
 
 
     public TcpNonBlockServer(int serverPort){
@@ -22,55 +29,52 @@ public final class TcpNonBlockServer implements IServer {
 
     @Override
     public void Start() {
-
-        //ServerSocket tcpSocket;
         try {
+            //initializes the server socket
             Initialize();
-            //tcpSocket = new ServerSocket(tcpPort);
-            //ServerSocketChannel serverChannel = tcpSocket.getChannel();
-            //Sets the socket as a non blocking.
-            //serverChannel.configureBlocking(false);
 
-            System.out.println("Tcp Non blocking server is running on port:" + this.tcpPort);
             //Listen to new connections
             Listener();
-            //Closes the connection
-            //tcpSocket.close();
         }
         catch (Exception excp){
             excp.printStackTrace();
-            return;
         }
-
     }
 
     @Override
     public void Stop() {
-        System.out.println("Tcp server is terminating");
+        System.out.println("Tcp Non blocking server is terminating");
         this.running = false;
+        try {
+            if(this.serverSocket != null && this.serverSocket.isClosed() == false){
+                this.serverSocket.close();
+            }
+
+            if (this.serverSocketChannel != null) {
+                serverSocketChannel.close();
+            }
+
+            this.selector.close();
+        }catch (Exception excp){
+            excp.printStackTrace();
+        }
     }
 
     private void Listener() {
-
-        ExecutorService threadPool = Executors.newCachedThreadPool();
         try {
             do {
-                //Accept any connection. by default its a blocking call until we have a new client
-                SocketChannel clientSocket = this.serverSocketChannel.accept();
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iter = selectedKeys.iterator();
+                while (iter.hasNext()) {
 
-                //Setting to non-blocking socket
-                clientSocket.configureBlocking(false);
-                Socket socket = clientSocket.socket();
-                System.out.println("Connection with " + socket + " has been established.\n");
-
-                this.serverSocketChannel.r
-                //socketChannel.register(selector, SelectionKey.OP_READ);
-                //selector.selectedKeys().remove(key);
-
-                //System.out.println("New client with the IP: " + clientSocket.getInetAddress().getHostAddress());
-                //Submit the task to a new thread.
-                //threadPool.submit(new TaskWorker(clientSocket));
-
+                    SelectionKey key = iter.next();
+                    //Calls register to accept the call
+                    if (key.isAcceptable()) {
+                        Register();
+                    }
+                    iter.remove();
+                }
             }while (this.running);
         }
         catch (Exception excp){
@@ -78,19 +82,33 @@ public final class TcpNonBlockServer implements IServer {
         }
     }
 
-    private void Initialize() throws Exception {
+    private void Register()
+            throws IOException {
+
+        SocketChannel client = this.serverSocketChannel.accept();
+        TaskWorker dispatcher = new TaskWorker(client.socket(), this);
+        this.threadPool.submit(dispatcher);
+    }
+
+    private void Initialize() {
         try {
-            System.out.println("Server Starting...\n");
+            this.selector = Selector.open();
 
             this.serverSocketChannel = ServerSocketChannel.open();
+            //Setting to non-blocking socket
             this.serverSocketChannel.configureBlocking(false);
-            serverSocket = serverSocketChannel.socket();
+
+            this.serverSocket = serverSocketChannel.socket();
             InetSocketAddress netAddress = new InetSocketAddress(this.tcpPort);
-            serverSocket.bind(netAddress);
+            this.serverSocket.bind(netAddress);
+
+            //unblock with accept
+            this.serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            //instanciates the threadpool to handle replies in a async way
+            this.threadPool = Executors.newCachedThreadPool();
+            System.out.println("The Server Starting in:" + netAddress.toString());
         } catch (Exception excp) {
             excp.printStackTrace();
         }
     }
-
-
 }
